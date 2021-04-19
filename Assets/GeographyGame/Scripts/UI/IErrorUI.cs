60d15f407b2cf62a67bc94c4977be0456f1b8659
HankSnouwaert <54607969+HankSnouwaert@users.noreply.github.com>
2021-04-19 14:54:00 -0400

    Update IErrorUI.cs
    Added comments

--- Assets/GeographyGame/Scripts/UI/IErrorUI.cs
+++ Assets/GeographyGame/Scripts/UI/IErrorUI.cs
@@ -2,10 +2,31 @@
 {
     public interface IErrorUI 
     {
+        /// <summary>
+        ///  Executed when the user closes the error UI
+        /// </summary>
         void errorUIClosed();
+        
+        /// <summary>
+        ///  Sets the error message displayed by the error UI
+        ///  <param name="message"></param> The error message for the user>
+        /// </summary>
         void setErrorMessage(string message);
+        
+        /// <summary>
+        ///  Sets the stack trace displayed by the error UI
+        ///  <param name="stackTrace"></param> The stack trace displayed to the user>
+        /// </summary>
         void setStackTrace(string stackTrace);
+
+        /// <summary>
+        ///  Opens the error UI
+        /// </summary>
         void OpenUI();
+
+        /// <summary>
+        ///  Closes the error UI
+        /// </summary>
         void CloseUI();
 
     }
