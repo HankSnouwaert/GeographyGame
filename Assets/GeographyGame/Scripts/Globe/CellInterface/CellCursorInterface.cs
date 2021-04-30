3d99c7fb2cf6953d70039652f000c327b6259b9b
HankSnouwaert <54607969+HankSnouwaert@users.noreply.github.com>
2021-04-30 16:47:45 -0400

    Refactored CellCursotInterface

--- Assets/GeographyGame/Scripts/Globe/CellInterface/CellCursorInterface.cs
+++ Assets/GeographyGame/Scripts/Globe/CellInterface/CellCursorInterface.cs
@@ -14,12 +14,40 @@ namespace WPM
         public ICellExiter CellExiter { get; set; } 
         public int highlightedCellIndex { get; set; }
         public Cell highlightedCell { get; set; }
+        //Error Checking
+        private InterfaceFactory interfaceFactory;
+        private IErrorHandler errorHandler;
+        private bool componentMissing = false;
 
-        void Awake()
+        private void Awake()
         {
-            CellClicker = GetComponent(typeof(ICellClicker)) as ICellClicker;
-            CellEnterer = GetComponent(typeof(ICellEnterer)) as ICellEnterer;
-            CellExiter = GetComponent(typeof(ICellExiter)) as ICellExiter;
+            interfaceFactory = FindObjectOfType<InterfaceFactory>();
+            if (interfaceFactory == null)
+                gameObject.SetActive(false);
+            try
+            {
+                CellClicker = GetComponent(typeof(ICellClicker)) as ICellClicker;
+                CellEnterer = GetComponent(typeof(ICellEnterer)) as ICellEnterer;
+                CellExiter = GetComponent(typeof(ICellExiter)) as ICellExiter;
+                if (CellClicker == null || CellEnterer == null || CellExiter == null)
+                    componentMissing = true;
+            }
+            catch
+            {
+                componentMissing = true;
+            }
+        }
+
+        private void Start()
+        {
+            errorHandler = interfaceFactory.ErrorHandler;
+            if (errorHandler == null)
+                gameObject.SetActive(false);
+            else
+            {
+                if (componentMissing == true)
+                    errorHandler.ReportError("Cell Cursor Interface component missing", ErrorState.restart_scene);
+            }
         }
     }
 }
