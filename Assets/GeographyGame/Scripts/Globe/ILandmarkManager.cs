60364afc90b39c1ee091bb61f552716a881a7555
HankSnouwaert <54607969+HankSnouwaert@users.noreply.github.com>
2021-05-06 23:38:20 -0400

    Added comments to LandmarkManager

--- Assets/GeographyGame/Scripts/Globe/ILandmarkManager.cs
+++ Assets/GeographyGame/Scripts/Globe/ILandmarkManager.cs
@@ -1,7 +1,14 @@
 ﻿namespace WPM
 {
+    /// <summary> 
+    /// Holds information and functionality relating to landmarks
+    /// </summary>
     public interface ILandmarkManager
     {
+        /// <summary> 
+        /// Instantiates the cultural landmark for a given mount point
+        /// </summary>
+        /// <param name="mountPoint"> The mount point having its cultural landmark instantiated</param>
         bool InstantiateCulturalLandmark(MountPoint mountPoint);
     }
 }
